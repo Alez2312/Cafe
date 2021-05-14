e347409c7e5ece880c5cbd52949fc2e409eff17c
Alez2312 <alejandropl2509@gmail.com>
2021-05-14 15:08:27 -0500

    Cafetería
    Aplicación para cafetería

---
+++ GoodCoffee-main/lib/Modelo/Product.dart
@@ -0,0 +1,112 @@
+class Product {
+  final String image, title, price;
+
+  Product({this.image, this.title, this.price});
+}
+
+List<Product> coffeesList = [
+  Product(
+    price: "\$3.900",
+    title: "Latté",
+    image: "assets/images/image1.jpg",
+  ),
+  Product(
+    price: "\$3.200",
+    title: "Expreso",
+    image: "assets/images/image2.jpg",
+  ),
+  Product(
+    price: "\$4.000",
+    title: "Capuchino",
+    image: "assets/images/image3.jpg",
+  ),
+  Product(
+    price: "\$3.900",
+    title: "Café negro",
+    image: "assets/images/image4.jpg",
+  ),
+  Product(
+    price: "\$4.000",
+    title: "Frappe",
+    image: "assets/images/image3.jpg",
+  ),
+  Product(
+    price: "\$4.200",
+    title: "Vainilla",
+    image: "assets/images/image3.jpg",
+  ),
+  Product(
+    price: "\$4.000",
+    title: "Cafe con leche",
+    image: "assets/images/image3.jpg",
+  ),
+];
+
+List<Product> drinksList = [
+  Product(
+    price: "\$3.000",
+    title: "Jugo de limon",
+    image: "assets/images/image1.jpg",
+  ),
+  Product(
+    price: "\$3.000",
+    title: "Jugo de naranja",
+    image: "assets/images/image2.jpg",
+  ),
+  Product(
+    price: "\$1.000",
+    title: "Agua Pura",
+    image: "assets/images/image3.jpg",
+  ),
+  Product(
+    price: "\$2.000",
+    title: "Tokis",
+    image: "assets/images/image4.jpg",
+  ),
+];
+
+List<Product> cakesList = [
+  Product(
+    price: "\$1.200",
+    title: " CheeseCake",
+    image: "assets/images/image1.jpg",
+  ),
+  Product(
+    price: "\$2.000",
+    title: "Chocolate",
+    image: "assets/images/image2.jpg",
+  ),
+  Product(
+    price: "\$4.000",
+    title: "Moka",
+    image: "assets/images/image3.jpg",
+  ),
+  Product(
+    price: "\$4.200",
+    title: "Vainilla",
+    image: "assets/images/image4.jpg",
+  ),
+];
+
+List<Product> sandwichesList = [
+  Product(
+    price: "\$1.500",
+    title: "Club",
+    image: "assets/images/image1.jpg",
+  ),
+  Product(
+    price: "\$1.500",
+    title: "Molletes",
+    image: "assets/images/image2.jpg",
+  ),
+  Product(
+    price: "\$2.000",
+    title: "Integral",
+    image: "assets/images/image3.jpg",
+  ),
+  Product(
+    price: "\$2.500",
+    title: "Vegano",
+    image: "assets/images/image4.jpg",
+  ),
+];
---
+++ GoodCoffee-main/lib/Values/StringApp.dart
@@ -0,0 +1,38 @@
+//String Menu
+
+String get shopStr => "GOOD COFFEE";
+String get coffeesStr => "Cafes";
+String get drinksStr => "Bebidas";
+String get cakesStr => "Pasteles";
+String get sandwichesStr => "Sandwiches";
+String get homeStr => "Explora";
+String get sectionMenuTitleStr => "Nuestra Carta";
+String get sectionMenuSubTitleStr => "Todo Organico";
+
+//String Drawer y footer
+String get aboutUsStr => "Nosotros";
+String get contactUsStr => "Contactanos";
+String get locationStr => "Ubicación";
+String get knowUsStr => "Conocenos";
+String get loginStr => "Iniciar Sesión";
+String get copyrightStr => "Copyright © 2021 | Alez2312";
+String get emailStr => "Correo";
+String get addressStr => "Direccion";
+String get paymentStr => "Pagos";
+String get cancellationStr => "Cancelación";
+String get fAQStr => "Preguntas frecuentes";
+
+//titles footer
+String get aboutStr => "SOBRE";
+String get helpStr => "AYUDA";
+String get socialStr => "REDES SOCIALES";
+
+//SOCIAL MEDIA
+String get twitterStr => "Twitter";
+String get facebookStr => "Facebook";
+String get instagramStr => "Instagram";
+
+//Datos default
+String get nameStr => "Alejandro Pérez";
+String get emailDefaultStr => "alejandropl2509@gmail.com";
+String get addressDefaultStr => "Medellín";
